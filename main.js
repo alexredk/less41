d6faaa1de659df3e1c66fc0f0d7dd20d6ded1fa5
alex <74060189+alexredk@users.noreply.github.com>
2021-05-20 23:40:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,137 @@
+// let a = +prompt('ВВедите число')
+// switch (a){
+//    case 0:
+//        alert('Верно')
+//     case 1:
+//         alert('Верно')
+//     case -3:
+//         alert('Верно')
+//         default:
+//             alert('Неверно')
+// }
+// 
+// let a = +prompt('ВВедите число')
+//  switch(a > 0){
+//      case true:
+//          alert('верно')
+//          case false:
+//              alert('Неверно')
+     
+//  }
+// let a = +prompt('ВВедите число')
+//  switch(a < 0){
+//      case true:
+//          alert('верно')
+//          case false:
+//              alert('Неверно')
+     
+//  }
+
+// let a = +prompt('Введите число')
+// switch(a){
+//     case a>0:
+//         alert('Верно')
+//     case a=0:
+//         alert('верно')
+//     default:
+//         alert('Неверно')
+// }
+// let a = +prompt('Введите число')
+// switch(a){
+//     case a<0:
+//         alert('Верно');
+//     case a=0:
+//         alert('верно');
+//     default:
+//         alert('Неверно');
+// }
+// let a = +prompt('Введите число')
+// switch(a < 0 && a == 0){
+//     case true:
+//         alert('Верно');
+//      case false:
+//             alert('Неверно');
+// }
+// let a = +prompt('Введите число')
+// switch(a == 0){
+//     case true:
+//         alert('Верно');
+//     case false:
+//     alert('Неверно');
+// }
+// let a = +prompt('Введите число')
+// switch(a == 1){
+//     case true:
+//         alert('верно');
+//     case false:
+//         alert('неверно')
+// }
+// let a = +prompt('Введите число')
+// switch(a){
+//     case "test" :
+//         alert('верно');
+//     case "тест" :
+//         alert('неверно');
+//         default:
+//             alert('Неверно')
+
+// }
+// let a = +prompt("ВВедите переменную")
+// if(a=0){
+//     alert('верно')
+// }
+// else{
+//     alert('неверно')
+// }
+// let a = +prompt('Введите число')
+// if(a > 0){
+//     alert('верно')
+// }
+// else{
+//     alert('Неверно')
+// }
+// let a = +prompt('Введите число')
+// if(a < 0){
+//     alert('Верно')
+// }
+// else{
+//     alert('неверно')
+// }
+// let a = +prompt('введите число')
+// if(a > 0 && a=0){
+//     alert('Верно')
+// }
+// else{
+//     alert('неверно')
+// }
+// let a = +prompt('Введите число')
+// if(a < 0){
+//     alert('неверно')
+// }
+// else if(a = 0){
+//     alert('неверно')
+// }
+// else{
+//     alert('верно')
+// }
+// let a = +prompt('введите число')
+// if(a !=0){
+//     alert('верно')
+// }
+// else{
+//     alert('неверно')
+// }
+// let a = +prompt('введите число')
+// if(a != 0){
+//     alert('верно')
+// }
+// else{
+//     alert('неверно')
+// }
+// let a = +prompt('введите число')
+// if(a = test){
+//     alert('верно')
+// }
+// else{
+//     alert('неверно')
+// }
